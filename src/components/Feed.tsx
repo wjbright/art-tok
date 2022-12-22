2a900bed2e37b8e8345a7136e5caba419c3fe275
Bright Williams <brightasima@gmail.com>
2022-12-22 17:25:20 +0100

    feat: feed

---
+++ src/components/Feed.tsx
@@ -0,0 +1,68 @@
+export const Feed = () => {
+    return <div className="flex flex-col h-screen min-w-full snap-always snap-mandatory snap-y overflow-y-scroll scroll-smooth">
+        <div className="min-h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+            <div className="flex flex-row min-h-screen min-w-full snap-always snap-mandatory snap-x overflow-x-scroll scroll-smooth">
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+                    one
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-blue-500 text-white">
+                    two
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-purple-500 text-white">
+                    three
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-green-500 text-white">
+                    four
+                </div>
+            </div>
+        </div>
+        <div className="min-h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+            <div className="flex flex-row min-h-screen min-w-full snap-always snap-mandatory snap-x overflow-x-scroll scroll-smooth">
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+                    one
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-blue-500 text-white">
+                    two
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-purple-500 text-white">
+                    three
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-green-500 text-white">
+                    four
+                </div>
+            </div>
+        </div>
+        <div className="min-h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+            <div className="flex flex-row min-h-screen min-w-full snap-always snap-mandatory snap-x overflow-x-scroll scroll-smooth">
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+                    one
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-blue-500 text-white">
+                    two
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-purple-500 text-white">
+                    three
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-green-500 text-white">
+                    four
+                </div>
+            </div>
+        </div>
+        <div className="min-h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+            <div className="flex flex-row min-h-screen min-w-full snap-always snap-mandatory snap-x overflow-x-scroll scroll-smooth">
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-red-500 text-white">
+                    one
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-blue-500 text-white">
+                    two
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-purple-500 text-white">
+                    three
+                </div>
+                <div className="flex items-center justify-center h-screen min-w-full snap-end snap-always bg-green-500 text-white">
+                    four
+                </div>
+            </div>
+        </div>
+    </div>
+}
