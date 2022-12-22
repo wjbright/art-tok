4c338253f912b31be9d54977edab4437ad325bca
Samuel Olamide <sam99kupo@gmail.com>
2022-12-22 17:22:01 +0100

    Add skelenton for authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,29 +1,24 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+  // callbacks: {
+  //   session({ session, user }) {
+  //     if (session.user) {
+  //       session.user.id = user.id;
+  //     }
+  //     return session;
+  //   },
+  // },
+
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    // ...add more providers here
   ],
 };
 
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,10 +1,12 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { CREATE_USER_VALIDATION } from "../validations/auth";
+import { registerUser } from "../controllers/auth.ctrl";
 
 export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+  registerUser: publicProcedure
+    .input(CREATE_USER_VALIDATION)
+    .mutation(async ({ input }) => registerUser(input)),
 });
