adf8fc9b1cb9ba07867ade7bb160d2fcd227f0ef
Bright Williams <brightasima@gmail.com>
2023-01-11 12:52:08 +0100

    pexels wip

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -3,19 +3,16 @@ import { trpc } from "../utils/trpc";
 import { Profile } from "./Profile"
 
 export const Feed = () => {
-    const profilePages = trpc.feed.getFeed.useInfiniteQuery({ limit: 2 }, { getNextPageParam: (lastPage) => lastPage.nextCursor });
+    const profilePages = trpc.feed.getFeed.useInfiniteQuery(
+        { limit: 2 },
+        { getNextPageParam: (lastPage) => lastPage.nextCursor }
+    );
     const [lastPageElementRef] = useInfiniteScroll(profilePages);
 
     if (profilePages.isLoading) return <span>Loading...</span>
 
     return (
         <div className="fixed flex flex-col flex-1 h-full w-screen">
-            <div className="flex flex-row justify-evenly items-center w-full h-16 bg-red-100">
-                <div>Search</div>
-                <div>Explore</div>
-                <div>Messages</div>
-                <div>Profile</div>
-            </div>
             <div className="flex flex-col h-full w-full snap-always snap-mandatory snap-y overflow-y-scroll scroll-smooth">
                 {profilePages?.data?.pages.map((pages) => pages.data.map((page, index) => {
                     if (profilePages.data.pages.length === index + 1) {
@@ -23,12 +20,6 @@ export const Feed = () => {
                     }
                     return <Profile pages={page.profilePages} key={index} />
                 }))}
-            </div>
-            <div className="flex flex-row justify-evenly items-center w-full h-16 bg-red-100">
-                <div>Search</div>
-                <div>Explore</div>
-                <div>Messages</div>
-                <div>Profile</div>
             </div>
         </div>
     )
--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -7,7 +7,6 @@ export default function useInfiniteScroll(trpcRoute: any) {
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0]?.isIntersecting && trpcRoute.hasNextPage) {
-          console.log("Shit is showing!!");
           trpcRoute.fetchNextPage();
         }
       });
